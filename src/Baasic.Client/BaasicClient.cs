e47b3e97a3f9de33fb78d1be3ffc359166ec15cd
khorvat <khorvat@icodeteam.net>
2014-02-16 16:31:37 +0100

    Working on Baasic Client

--- src/Baasic.Client/BaasicClient.cs
+++ src/Baasic.Client/BaasicClient.cs
@@ -25,7 +25,6 @@ namespace Baasic.Client
         public const string HalJsonMediaType = "application/hal+json";
         
         public const string BaasicBaseAddress = "http://api.baasic.com/v1";
-        public const string BaasicSSLBaseAddress = "https://api.baasic.com/v1";
         public TimeSpan BaasicrDefaultTimeout = TimeSpan.FromSeconds(10);
         #endregion
 
@@ -34,6 +33,49 @@ namespace Baasic.Client
         /// Gets or sets the application identifier.
         /// </summary>
         public string ApplicationIdentifier { get; protected set; }
+
+        private string _baseAddress;
+
+        public string BaseAddress
+        {
+            get 
+            {
+                if (String.IsNullOrWhiteSpace(_baseAddress))
+                    _baseAddress = BaasicBaseAddress;
+                return _baseAddress; 
+            }
+            set 
+            { 
+                _baseAddress = value;
+                _secureBaseAddress = null;
+            }
+        }
+
+        private string _secureBaseAddress;
+
+        public string SecureBaseAddress
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(_secureBaseAddress))
+                    _secureBaseAddress = BaseAddress.Replace("http://", "https://");
+                return _secureBaseAddress;
+            }
+            set { _secureBaseAddress = value; }
+        }
+
+        private string _defaultMediaType;
+
+        public string DefaultMediaType
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(_defaultMediaType))
+                    return HalJsonMediaType;
+                return _defaultMediaType;
+            }
+            set { _defaultMediaType = value; }
+        }
         #endregion
 
         #region Constructor
@@ -44,6 +86,17 @@ namespace Baasic.Client
         public BaasicClient(string applicationIdentifier)
         {
             ApplicationIdentifier = applicationIdentifier;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="baseAddress">Baasic API address.</param>
+        /// <param name="applicationIdentifier">Application identifier.</param>
+        public BaasicClient(string baseAddress, string applicationIdentifier)
+        {
+            ApplicationIdentifier = applicationIdentifier;
+            BaseAddress = baseAddress;
         } 
         #endregion
 
@@ -61,9 +114,14 @@ namespace Baasic.Client
         
         public Task<HttpResponseMessage> DeleteAsync(Uri requestUri, CancellationToken cancellationToken)
         { return null; }
-        
+
         public Task<HttpResponseMessage> GetAsync(string requestUri)
-        { return null; }
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return client.SendAsync(CreateRequest(requestUri, DefaultMediaType, HttpMethod.Get));
+            }
+        }
         
         public Task<HttpResponseMessage> GetAsync(Uri requestUri)
         { return null; }
@@ -135,6 +193,27 @@ namespace Baasic.Client
             //result.Headers
             return result;
         }
+
+        public string GetApiUrl(string relativeUrl, params object[] parameters)
+        {
+            return GetApiUrl(false, ApplicationIdentifier, relativeUrl, parameters);
+        }
+
+        public string GetSecureApiUrl(string relativeUrl, params object[] parameters)
+        {
+            return GetApiUrl(true, ApplicationIdentifier, relativeUrl, parameters);
+        }
+
+        private string GetApiUrl(bool ssl, string applicationIdentifier, string relativeUrl, params object[] parameters)
+        {
+            return GetApiUrl(ssl, applicationIdentifier.TrimEnd('/') + "/" + relativeUrl, parameters);
+        }
+
+        public string GetApiUrl(bool ssl, string relativeUrl, params object[] parameters)
+        {
+            return String.Format("{0}/{1}", ssl ? SecureBaseAddress.TrimEnd('/') : BaseAddress.TrimEnd('/'), String.Format(relativeUrl, parameters));
+        }
+
         #endregion
     }
 }
