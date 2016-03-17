6580eac227b4c25e6f22648145b6d1c4e14d5656
Kristijan Horvat <kristijan@mono.hr>
2016-03-17 15:49:13 +0100

    Created NuGet packages for .NET SDK

--- src/Baasic.Client.Common/Properties/AssemblyInfo.cs
+++ src/Baasic.Client.Common/Properties/AssemblyInfo.cs
@@ -1,30 +1,23 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Baasic.Client.Common")]
-[assembly: AssemblyDescription("")]
+// General Information about an assembly is controlled through the following set of attributes. Change these attribute
+// values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("Baasic .NET SDK Common")]
+[assembly: AssemblyDescription("Baasic .NET SDK Common Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Baasic.Client.Common")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Mono Ltd.")]
+[assembly: AssemblyProduct("Baasic .NET SDK Common")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
+// 
+// Major Version Minor Version Build Number Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.8.100")]
+[assembly: AssemblyFileVersion("1.0.8.100")]
--- src/Baasic.Client.Model/Properties/AssemblyInfo.cs
+++ src/Baasic.Client.Model/Properties/AssemblyInfo.cs
@@ -1,29 +1,21 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Baasic.Client.Model")]
-[assembly: AssemblyDescription("")]
+// General Information about an assembly is controlled through the following set of attributes. Change these attribute
+// values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("Baasic .NET SDK Models")]
+[assembly: AssemblyDescription("Baasic .NET SDK Models")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Baasic.Client.Model")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Mono Ltd.")]
+[assembly: AssemblyProduct("Baasic .NET SDK Models")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-
 // Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
+// 
+// Major Version Minor Version Build Number Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.5")]
-[assembly: AssemblyFileVersion("1.0.1.5")]
+[assembly: AssemblyVersion("1.0.8.100")]
+[assembly: AssemblyFileVersion("1.0.8.100")]
