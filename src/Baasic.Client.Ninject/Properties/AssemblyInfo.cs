b55974b308094febefe85d8e831b167a1a8a1b3b
Kristijan Horvat <kristijan@mono.hr>
2016-03-22 11:55:26 +0100

    Implemented Ninject composition root

---
+++ src/Baasic.Client.Ninject/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following set of attributes. Change these attribute
+// values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("Baasic .NET SDK Ninject")]
+[assembly: AssemblyDescription("Baasic .NET SDK Ninject Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mono Ltd.")]
+[assembly: AssemblyProduct("Baasic .NET SDK Ninject")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+// Major Version Minor Version Build Number Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.8.100")]
+[assembly: AssemblyFileVersion("1.0.8.100")]
