215854c30648f23f6bfd1608dac2a2382c222753
Kristijan Horvat <kristijan@mono.hr>
2016-04-19 13:08:25 +0200

    The bug related to Ninject type to type registration is fixed Unit tests are fixed

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -15,5 +15,5 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.111")]
-[assembly: AssemblyFileVersion("0.8.111")]
+[assembly: AssemblyVersion("0.8.112")]
+[assembly: AssemblyFileVersion("0.8.112")]
