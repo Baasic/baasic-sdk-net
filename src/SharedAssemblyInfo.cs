b6890e371e31c147c72f3daa12dfb6c836cc4b0b
Kristijan Horvat <kristijan@mono.hr>
2016-08-11 16:45:24 +0200

    Minor fix on the NuGet specs

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -15,8 +15,8 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.137")]
-[assembly: AssemblyFileVersion("0.8.137")]
+[assembly: AssemblyVersion("0.8.139")]
+[assembly: AssemblyFileVersion("0.8.139")]
 #if DEBUG
-[assembly: AssemblyInformationalVersion("0.8.137-alpha")]
+[assembly: AssemblyInformationalVersion("0.8.139-alpha")]
 #endif
