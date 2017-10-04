b016f96668bea1a1734cf7e6949a01587a6a8248
Kristijan Horvat <kristijan@mono.hr>
2017-10-04 14:29:33 +0200

    Working on AutoFac integration

---
+++ src/Baasic.Client.AutoFac/AutoFacDependencyResolver.cs
@@ -0,0 +1,154 @@
+﻿using Baasic.Client.Common.Infrastructure.DependencyInjection;
+using Baasic.Client.Infrastructure.DependencyInjection;
+using Autofac;
+using System;
+using System.Collections.Generic;
+
+namespace Baasic.Client.AutoFac
+{
+    /// <summary>
+    /// Baasic client dependency resolver.
+    /// </summary>
+    public class AutofacDependencyResolver : IDependencyResolver
+    {
+        private ContainerBuilder builder { get; set; }
+        
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacDependencyResolver" /> class.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        public AutofacDependencyResolver(IContainer container)
+        {
+            Container = container;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the container.
+        /// </summary>
+        /// <value>The container.</value>
+        private IContainer Container { get; set; }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// Gets the service.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <returns></returns>
+        public object GetService(Type serviceType)
+        {
+            return Container.Resolve(serviceType);
+        }
+
+        /// <summary>
+        /// Gets the service.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T GetService<T>()
+        {
+            return Container.Resolve<T>();
+        }
+
+        /// <summary>
+        /// Gets the services.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <returns></returns>
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return Kernel.GetAll(serviceType);
+        }
+
+        /// <summary>
+        /// Gets the services.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public IEnumerable<T> GetServices<T>()
+        {
+            return Kernel.GetAll<T>();
+        }
+
+        /// <summary>
+        /// Initializes the specified modules.
+        /// </summary>
+        /// <exception cref="System.ArgumentNullException">modules</exception>
+        public void Initialize()
+        {
+            Kernel.Bind(x =>
+{
+    x.FromAssembliesMatching("Baasic.Client.dll", "Baasic.Client.*.dll")
+     .SelectAllClasses()
+     .InheritedFrom<IDIModule>()
+     .BindDefaultInterface();
+});
+
+            IEnumerable<IDIModule> modules = Kernel.GetAll<IDIModule>();
+
+            foreach (IDIModule module in modules)
+            {
+                module.Load(this);
+            }
+        }
+
+        /// <summary>
+        /// Registers the specified service type.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="activator">The activator.</param>
+        public void Register(Type serviceType, Func<object> activator)
+        {
+            Kernel.Rebind(serviceType).ToMethod(ctx => activator);
+        }
+
+        /// <summary>
+        /// Registers the specified service type to implementation type.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="implementationType">Type of the implementation.</param>
+        public void Register(Type serviceType, Type implementationType)
+        {
+            Kernel.Rebind(serviceType).To(implementationType);
+        }
+
+        /// <summary>
+        /// Registers the specified activator.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="activator">The activator.</param>
+        public void Register<T>(Func<T> activator) where T : class
+        {
+            Kernel.Rebind(typeof(T)).ToMethod(ctx => activator);
+        }
+
+        /// <summary>
+        /// Registers this instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="I"></typeparam>
+        public void Register<T, I>()
+            where I : class, T
+        {
+            Kernel.Rebind<T>().To<I>();
+        }
+
+        #endregion Methods
+    }
+}
