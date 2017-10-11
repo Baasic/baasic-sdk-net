2aec68a12f5c109d82ff14b6f3325cda09c8c516
Željko Huber <zeljko@mono.hr>
2017-10-11 09:53:35 +0200

    - version increment

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -15,8 +15,8 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.154")]
-[assembly: AssemblyFileVersion("0.8.154")]
+[assembly: AssemblyVersion("0.8.155")]
+[assembly: AssemblyFileVersion("0.8.155")]
 #if DEBUG
-[assembly: AssemblyInformationalVersion("0.8.154-alpha")]
+[assembly: AssemblyInformationalVersion("0.8.155-alpha")]
 #endif
