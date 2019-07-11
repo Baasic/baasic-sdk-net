84574a72a1748e55a81e4b07c23df50c54151b6f
Davor Kolarević <dkolarevic@mono-software.com>
2019-07-11 15:27:19 +0200

    Fix response cookie reading

--- src/Baasic.Client.WebHost/Infrastructure/Security/CookieTokenHandler.cs
+++ src/Baasic.Client.WebHost/Infrastructure/Security/CookieTokenHandler.cs
@@ -1,10 +1,12 @@
 ﻿using Baasic.Client.Common.Infrastructure.Security;
 using System;
 using System.Threading;
+using System.Linq;
 
 #if NET461 || NET462
 
 using System.Linq;
+
 using System.Web;
 
 #else
@@ -219,13 +221,21 @@ namespace Baasic.Client.Infrastructure.Security
         private string GetCookieValueFromResponse(HttpResponse response, string cookieName)
         {
             foreach (var headers in response.Headers.Values)
+            {
                 foreach (var header in headers)
-                    if (header.StartsWith(cookieName))
+                {
+                    var startIndex = header.LastIndexOf(cookieName);
+                    if (startIndex != -1)
                     {
+                        var cookie = header.Substring(startIndex);
                         var p1 = header.IndexOf('=');
                         var p2 = header.IndexOf(';');
+
                         return header.Substring(p1 + 1, p2 - p1 - 1);
                     }
+                }
+            }
+
             return null;
         }
 #endif
